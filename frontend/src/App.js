ae819508d36f5ea45b14a250d86e162d0efc3e3e
rubenrgc <rubenrgc@hotmail.com>
2023-02-04 18:01:58 -0800

    se hizo el frontend y se empezo a hacer el backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
-//faltaria import react from react
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
     <>
-      <h1>Welcome To Proshop</h1>
+      <main className='py-6'>
+        <Router>
+          <Header />
+        
+            <Container>
+              <Routes>
+                <Route path='/' element={<HomeScreen />} exact />
+                <Route path='/product/:id' element={<ProductScreen />} />
+                
+              </Routes>
+            </Container>
+       
+          <Footer />
+        </Router>
+      </main>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
