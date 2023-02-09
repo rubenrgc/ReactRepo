8ead37e229350fd9de8e61ae435573fc154bbcb0
rubenrgc <rubenrgc@hotmail.com>
2023-02-08 17:54:42 -0800

    backend productScreen redux actions constanst homescrreen

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const connectDB = async () => {
-try {
-    mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGO_URI,{
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
- 
+  try {
+    mongoose.set('strictQuery', false)
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
     })
 
-console.log(`MongoDB Connected: ${conn.connection.host}`)
-
-
-} catch (error) {
-    console.error(`Error: ${error.message}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
-}
+  }
 }
 
 export default connectDB
